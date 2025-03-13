779f2e685c6e3089cc42f528dc809644bb3d70fe
wakamatsu-misa <misamisaoor@gmail.com>
2025-03-13 10:40:11 +0900

    修正した

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -18,7 +18,7 @@
     $score9 = 30;
     $score10 = 60;
     
-    $total=$score1 + $score2 + $score3 + $score4 + $score4 + $score5 
+    $total=$score1 + $score2 + $score3  + $score4 + $score5 
     + $score6 + $score7 + $score8 + $score9 +$score10;
     
     $average = $total / 10;
